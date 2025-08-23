229ecf3285595e60fdb1d96536fc8ea19655343f
Roger Guilherme Da Silva <rogerguilhermedasilva31@gmail.com>
2025-08-23 16:00:54 -0300

    Update inserir dados.sql

--- inserir dados.sql
+++ inserir dados.sql
@@ -1,40 +1,25 @@
-INSERT INTO alunos (
-    alun_id,name, email,curso,Periodo
-) VALUES 
-('Aluno2025', 'Aluno Silva' 'alunos@gmail.com'), 
-('DS', 'Noturno', );
+/* Inserir alunos */
+INSERT INTO alunos (name, email, curso, periodo) VALUES
+('Aluno Silva', 'alunos@gmail.com', 'DS', 'Noturno'),
+('Joao Souza', 'joao@gmail.com', 'DS', 'Noturno');
 
+/* Inserir cursos */
+INSERT INTO cursos (nome_curso, nivel, tempo, periodo) VALUES
+('Desenvolvimento de Sistemas', 'Tecnologo', '3 anos', 'Noturno'),
+('Engenharia de Software', 'Tecnologo', '3 anos', 'Noturno');
 
-INSERT INTO cursos (
-    Instituição,curso_id,Nivel,Tempo,Periodo
-) VALUES (
-    ('IF Capivari', 'DSNoturno' 'Tecnologo'), 
-'3Anos', 'Noturno',    
-);
+/* Inserir aulas */
+INSERT INTO aulas (materia, horario, avaliacoes, alun_id, prof) VALUES
+('Banco de Dados 1', '19:00 às 22:30', 'Provas e Trabalhos', 1, 'Moises Francisco Olimpio Filho'),
+('Sistemas Operacionais', '19:00 às 22:30', 'Provas e Trabalhos', 1, 'Edivaldo Serafim');
 
+/* Inserir feedback */
+INSERT INTO feedback (title, curso_id, alun_id) VALUES
+('A matéria de BD é incrível', 1, 1),
+('Professores que torcem para o São Paulo são os melhores', 1, 2);
 
-INSERT INTO aulas (
-    Materias,horario,avaliações,alun_id,prof
-) VALUES (
-    ('BANCO DE DADOS 1, SISTEMAS OPERACIONAIS,ESTRUTURA DE DADOS 1,PROGRAMAÇÃO ORIENTADA A OBJETOS 1,ENGENHARIA DE SOFTWARE 1, INTERAÇÃO HUMANO-COMPUTADOR', '19:00 ás 22:30' 'Provas e Trabalhos',
-'aluno2025', '
-Moises Francisco Olimpio Filho,
-Edivaldo Serafim,Vitor Brandi Junior,
-Rafael Wendel Pinheiro,
-Waldo Luis de Lucca' );    
-);
-
-INSERT INTO feedback(
-    feed_id,title,curso_id,alun_id
-) VALUES 
-('a Materia de BD é incrivel', 2), 
-('Professores que Torcem para o São Paulo são os Melhores', 2);
-
-
-INSERT INTO comment (
-    comment_id, comment, feed_id, alun_id
-) VALUES 
-('o Professor Moises é muito sábio ', 2, 1), 
-('Alguns alunos faltaram', 1, 1), 
-(' São os Maiores do Brasil', 3, 2);
-
+/* Inserir comentários */
+INSERT INTO comment (comment, feed_id, alun_id) VALUES
+('O Professor Moises é muito sábio', 1, 1),
+('Alguns alunos faltaram', 1, 2),
+('São os maiores do Brasil', 2, 2);
