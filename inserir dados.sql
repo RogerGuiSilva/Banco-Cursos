ebe93104f2f2f188e8f9a2535ff9ce72d01be94b
Roger Guilherme Da Silva <rogerguilhermedasilva31@gmail.com>
2025-08-30 15:54:45 -0300

    Update inserir dados.sql

--- inserir dados.sql
+++ inserir dados.sql
@@ -1,25 +1,87 @@
-/* Inserir alunos */
-INSERT INTO alunos (name, email, curso, periodo) VALUES
-('Aluno Silva', 'alunos@gmail.com', 'DS', 'Noturno'),
-('Joao Souza', 'joao@gmail.com', 'DS', 'Noturno');
 
-/* Inserir cursos */
+/* 
+   INSERT cursos
+ */
 INSERT INTO cursos (nome_curso, nivel, tempo, periodo) VALUES
-('Desenvolvimento de Sistemas', 'Tecnologo', '3 anos', 'Noturno'),
-('Engenharia de Software', 'Tecnologo', '3 anos', 'Noturno');
+('Técnico em Alimentos', 'Técnico', 'Variável', 'Concomitante/Subsequente'),
+('Técnico em Recursos Humanos', 'Técnico', 'Variável', 'Concomitante/Subsequente (EAD com aulas presenciais)'),
+('Tecnologia em Análise e Desenvolvimento de Sistemas', 'Tecnologo', '3 anos', 'Noturno'),
+('Tecnologia em Processos Químicos', 'Tecnologo', '3 anos', 'Noturno'),
+('Tópicos de Matemática Elementar I', 'Extensão', 'Curta Duração', 'Presencial'),
+('Auxiliar Administrativo', 'Extensão', 'Curta Duração', 'EAD'),
+('Outros cursos de curta duração', 'Extensão', 'Curta Duração', 'Variável');
 
-/* Inserir aulas */
-INSERT INTO aulas (materia, hora_inicio, hora_fim, avaliacoes, alun_id, prof) VALUES
-('Banco de Dados 1', '19:00:00', '22:30:00', 'Provas e Trabalhos', 1, 'Moises Francisco Olimpio Filho'),
-('Sistemas Operacionais', '19:00:00', '22:30:00', 'Provas e Trabalhos', 1, 'Edivaldo Serafim');
+/*
+   INSERT alunos
+*/
+INSERT INTO alunos (matricula, name, email, curso, semestre, periodo) VALUES
+('CV3096629', 'Amanda Bueno Machado', 'amanda@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3095215', 'Andrei Luis Livramento dos Santos', 'andrei@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3095169', 'Andre Vitor Chaves Franco', 'andre@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3095479', 'Brendo Enzo de Souza', 'brendo@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3102882', 'Bruna de Fatima Inacio', 'bruna@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3043983', 'Bruno Ramos Moreira da Silva', 'bruno@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3095321', 'Caio Herling Spadotto', 'caioh@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV309510X', 'Caio Kenshin Souza Yamada', 'caio@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3102564', 'Carlos Eduardo Costa Menezes', 'carlos@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3096475', 'Caua Afonso Carvalho', 'caua@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3096866', 'Davi dos Santos Carvalho de Souza', 'davi@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3095401', 'Eduardo Leite Braz', 'eduardo@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3103234', 'Enrico Berno Totti', 'enrico@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3102599', 'Felipe Aguiar Pires', 'felipea@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3096998', 'Felipe Carrillo Dissero', 'felipec@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3096505', 'Felipe de Oliveira Carvalho', 'feliped@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3096556', 'Francisco Pereira Neto', 'francisco@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3096726', 'Guilherme Falcao Santos', 'guilhermef@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3100022', 'Guilherme Galdino Cupertino', 'guilhermeg@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3103242', 'João Pedro dos Santos', 'joaopedro@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3098184', 'Joey Fellipe Kartowidjojo', 'joey@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3096939', 'Joice Cristina Santos Marques', 'joice@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3104427', 'Josimeire Gomes Dantas Soares', 'josimeire@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3098877', 'Julya Silva de Oliveira', 'julya@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3095495', 'Manuela Aguiar Gil', 'manuela@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3095126', 'Marcelo Heiffig Junior', 'marcelo@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3096815', 'Matheus de Albuquerque Rocco', 'matheus@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV309670X', 'Matheus Ferreira de Souza', 'matheusf@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3096807', 'Mauricio Gomes de Campos Junior', 'mauricio@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3095282', 'Murilo Estefano Catule de Almeida', 'murilo@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV309698X', 'Pedro Augusto Figueredo', 'pedro@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3102645', 'Pietra Diez Assalin', 'pietra@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3102891', 'Ramon Moratto da Silva', 'ramon@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3095568', 'Renan Andrei da Silva', 'renan@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3095436', 'Rikelmy Lucena da Silva', 'rikelmy@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3096611', 'Roger Guilherme da Silva', 'roger@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3096424', 'Thiago Luis Goulart', 'thiago@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3102653', 'Vinicius Cavallari Nascimento', 'vinicius@gmail.com', 'DS', '2º semestre', 'Noturno'),
+('CV3025195', 'Walker Alessandro de Souza', 'walker@gmail.com', 'DS', '2º semestre', 'Noturno');
 
-/* Inserir feedback */
-INSERT INTO feedback (title,alun_id) VALUES
-('A matéria de BD é incrível', 1, 1),
-('Professores que torcem para o São Paulo são os melhores', 1, 2);
+/*
+   INSERT aulas
+*/
+INSERT INTO aulas (dia, materia, hora_inicio, hora_fim, avaliacoes, alun_id, prof) VALUES
+('Segunda Feira', 'SISTEMAS OPERACIONAIS', '19:00:00', '22:30:00', 'Provas e Trabalhos', 'CV3096611', 'Edivaldo Serafim'),
+('Terça Feira', 'BANCO DE DADOS', '19:00:00', '22:30:00', 'Provas e Trabalhos', 'CV3102653', 'Moises Francisco Olimpio Filho'),
+('Quarta Feira', 'INTERAÇÃO HUMANO-COMPUTADOR', '19:00:00', '20:40:00', 'Provas e Trabalhos', 'CV3095169', 'Moises Francisco Olimpio Filho'),
+('Quarta Feira', 'ENGENHARIA DE SOFTWARE', '21:00:00', '22:30:00', 'Provas e Trabalhos', 'CV3102891', 'Waldo Luis de Lucca'),
+('Quinta Feira', 'PROGRAMAÇÃO ORIENTADA A OBJETOS', '21:00:00', '22:30:00', 'Provas e Trabalhos', 'CV3095436', 'Rafael Wendel Pinheiro'),
+('Sexta Feira', 'ESTRUTURA DE DADOS 1', '19:00:00', '22:30:00', 'Provas e Trabalhos', 'CV3103234', 'Vitor Brandi Junior');
 
-/* Inserir comentários */
+/* 
+   INSERT feedback
+*/
+INSERT INTO feedback (title, description, curso_id, alun_id) VALUES
+('Aula de Banco de Dados', 'Meu sonho era ter aula de BD.', 1, 'CV3103234'),
+('Sistemas Operacionais', 'Adoro as aulas do Professor Edvaldo.', 1, 'CV3095126'),
+('Estrutura de Dados', 'Conteúdo um pouco difícil, mas interessante.', 1, 'CV3095169'),
+('Engenharia de Software', 'Estou gostando muito da matéria.', 1, 'CV3102891'),
+('Programação Orientada a Objetos', 'Java é Vida.', 1, 'CV3095436');
+
+/* 
+   INSERT comments
+*/
 INSERT INTO comments (comment, feed_id, alun_id) VALUES
-('O Professor Moises é muito sábio', 1, 1),
-('Alguns alunos faltaram', 1, 2),
-('São os maiores do Brasil', 2, 2);
+('Também adoro Banco de Dados, o professor explica muito bem!', 1, 'CV3096611'),
+('Concordo, o Professor Edvaldo é excelente nas explicações.', 2, 'CV309698X'),
+('Sim, Estrutura de Dados é desafiadora mas muito importante.', 3, 'CV3098184'),
+('Gostei da matéria também.', 4, 'CV3096424'),
+('Java realmente é vida, aprendizado valioso!', 5, 'CV3102564');
