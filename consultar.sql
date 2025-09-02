73e9eaa962a00abf5fea61dbf2d3c6800751ccce
Roger Guilherme Da Silva <rogerguilhermedasilva31@gmail.com>
2025-09-02 20:13:05 -0300

    Update consultar.sql

--- consultar.sql
+++ consultar.sql
@@ -1,25 +1,19 @@
 
-/* 
-   Consultar dados do aluno pela matrícula
- */
+/*  Consultar dados do aluno pela matrícula*/
 
 SELECT matricula, name, email, curso, semestre, periodo, created_at
 FROM alunos
 WHERE matricula = 'Digite a matricula';
 
 
-/* 
-   2️ Consultar aulas de um professor pelo nome
- */
+/* Consultar aulas de um professor pelo nome*/
 
 SELECT dia, materia, hora_inicio, hora_fim, avaliacoes
 FROM aulas
 WHERE prof = 'Digite o nome do Professor';
 
 
-/* 
-   Consultar feedbacks e comentários de um aluno pela matrícula
-*/
+/* Consultar feedbacks e comentários de um aluno pela matrícula*/
 
 SELECT 
     f.title AS feedback_titulo,
@@ -32,26 +26,20 @@ LEFT JOIN alunos a_c ON c.alun_id = a_c.matricula
 WHERE f.alun_id = 'Digite a Matricula';
 
 
-/*
-    Consulta de todos os alunos do curso de DS
-*/
+/*Consulta de todos os alunos do curso de DS*/
 SELECT matricula, name, email, semestre, periodo
 FROM alunos
 WHERE curso = 'DS';
 
 
-/* 
-    Consulta de todas as aulas de um determinado dia
-*/
+/*  Consulta de todas as aulas de um determinado dia*/
 
 SELECT dia, materia, hora_inicio, hora_fim, prof
 FROM aulas
 WHERE dia = 'digite o dia';
 
 
-/* 
-    Consulta de feedback e comentários de todos os alunos do curso de DS
- */
+/* Consulta de feedback e comentários de todos os alunos do curso de DS*/
 SELECT 
     f.title AS feedback_titulo,
     f.description AS feedback_descricao,
